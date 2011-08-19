f2ef1125abb0361436f5a70109f2d7717d04ab10
James wachira <jamomathenge@yahoo.com>
2011-08-18 22:33:27 -0700

    -create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])    
     if @user.save
       flash[:notice] = 'User was successfully created.'
-      redirect_to(@user)
+      redirect_to users_path
     else
       render :action => "new"
     end
